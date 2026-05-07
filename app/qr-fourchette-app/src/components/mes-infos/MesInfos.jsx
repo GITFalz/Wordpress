c31279ee005cfd8bdee59e92997f58a1388f81ae
GITFalz <153421988+GITFalz@users.noreply.github.com>
2026-05-07 12:45:12 +0200

    deleted everything to start fresh

--- app/qr-fourchette-app/src/components/mes-infos/MesInfos.jsx
+++
@@ -1,8 +0,0 @@
-export default function MesInfos({ user }) {
-    return (
-        <div>
-            <h2 className="text-xl font-bold mb-4">Mes Informations</h2>
-            {/* Contenu des informations de l'utilisateur */}
-        </div>
-    );
-}
--- app/qr-fourchette-app/src/components/qr-code/QRCode.jsx
+++
@@ -1,9 +0,0 @@
-import { QRCodeCanvas } from "qrcode.react";
-
-export default function QRCode({ user, link }) {
-    return (
-        <div>
-             <QRCodeCanvas value={link} size={200} />;
-        </div>
-    );
-}
